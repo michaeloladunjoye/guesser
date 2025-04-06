b7f7ddb7290c6a7d62b4ab3c007c1f8112ac218c
Michael Oladunjoye <testymicheal@gmail.com>
2025-04-06 03:45:43 -1200

    first commit

---
+++ java.js
@@ -0,0 +1,23 @@
+function checkGuess() {
+  let secretNumber = Math.floor(Math.random() * 10) + 1;
+  const guessInput = document.getElementById("guessInput");
+  const guess = parseInt(guessInput.value);
+  const resultMessage = document.getElementById("resultMessage");
+  const result = document.getElementById("result");
+
+  if (secretNumber === guess) {
+    console.log("Correct");
+    result.textContent = secretNumber;
+    resultMessage.textContent = "Correct! Guessed the number";
+    result.style.color = "green";
+    resultMessage.style.color = "green";
+  } else {
+    console.log("Wrong");
+    result.textContent = "The correct answer was " + secretNumber;
+    resultMessage.textContent = "Wrong! Try again.";
+    result.style.color = "red";
+    resultMessage.style.color = "red";
+  }
+  //   console.log("This is the secret number " + secretNumber);
+  //   console.log("This is the guess input " + guessInput);
+}
